62b28179fd5e853b35cbd9f5b30c70134b00e027
niraj <nirajbhayera4@gmail.com>
2026-09-01 00:42:24 +0530

    comprehensions techniques in python

---
+++ python/learn.py
@@ -0,0 +1,23 @@
+#to write the code shorter and cleaner 
+#we use comprehensions 
+
+#THE THREE CONCEPTS ARE :
+#1. COMPREHENSIONS 
+#2. LAMBDA FUNCTIONS
+#3. MAP, FILTER, REDUCE
+
+#1. COMPREHENSIONS 
+
+#there are three types of comprehensions 
+        #1. list comprehension
+
+        #2. set comprehension
+        #3. dictionary comprehension
+        
+        
+#normal way of writing a list is :
+numbers=[1,2,3]
+ans=[]
+for i in numbers :
+    ans.append(i*i)
+print(ans)
